848ae6116b852362373653a5815d460f92673970
Andrew Walbran <qwandor@google.com>
2023-04-12 17:47:32 +0100

    Add module documentation and example.

--- src/gicv3.rs
+++ src/gicv3.rs
@@ -2,6 +2,8 @@
 // This project is dual-licensed under Apache 2.0 and MIT terms.
 // See LICENSE-APACHE and LICENSE-MIT for details.
 
+//! Driver for the Arm Generic Interrupt Controller version 3 (or 4).
+
 mod registers;
 
 use self::registers::{GicdCtlr, Waker, GICD, GICR, SGI};
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,44 @@
 // This project is dual-licensed under Apache 2.0 and MIT terms.
 // See LICENSE-APACHE and LICENSE-MIT for details.
 
+//! Driver for the Arm Generic Interrupt Controller version 3 or 4, on aarch64.
+//!
+//! This top level module contains functions that are not specific to any particular interrupt
+//! controller, as support for other GIC versions may be added in future.
+//!
+//! # Example
+//!
+//! ```
+//! use arm_gic::{
+//!     gicv3::{GicV3, IntId, SgiTarget},
+//!     irq_enable,
+//! };
+//!
+//! // Base addresses of the GICv3 distributor and redistributor.
+//! const GICD_BASE_ADDRESS: *mut u64 = 0x800_0000 as _;
+//! const GICR_BASE_ADDRESS: *mut u64 = 0x80A_0000 as _;
+//!
+//! // Initialise the GIC.
+//! let mut gic = unsafe { GicV3::new(GICD_BASE_ADDRESS, GICR_BASE_ADDRESS) };
+//! gic.setup();
+//!
+//! // Configure an SGI and then send it to ourself.
+//! let sgi_intid = IntId::sgi(3);
+//! GicV3::set_priority_mask(0xff);
+//! gic.set_interrupt_priority(sgi_intid, 0x80);
+//! gic.enable_interrupt(sgi_intid, true);
+//! irq_enable();
+//! GicV3::send_sgi(
+//!     sgi_intid,
+//!     SgiTarget::List {
+//!         affinity3: 0,
+//!         affinity2: 0,
+//!         affinity1: 0,
+//!         target_list: 0b1,
+//!     },
+//! );
+//! ```
+
 #![no_std]
 
 pub mod gicv3;
