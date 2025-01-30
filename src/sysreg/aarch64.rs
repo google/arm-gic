268700256b7fa29e0759e9c43e75cece2c1afef6
Andrew Walbran <qwandor@google.com>
2025-01-30 14:11:15 +0000

    Fix formatting in macro.

--- src/sysreg/aarch64.rs
+++ src/sysreg/aarch64.rs
@@ -8,7 +8,7 @@
 macro_rules! read_sysreg32 {
     ($sysreg:ident, $opc1:literal, $crm:ident, $crn:ident, $opc2: literal, $function_name:ident) => {
         pub fn $function_name() -> u32 {
-            let value:u64;
+            let value: u64;
             // SAFETY: The caller of the macro guarantees that this system register is safe to read.
             unsafe {
                 core::arch::asm!(
